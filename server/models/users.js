dc897dcc1e8debab5c013ec37067ffb077d91a4a
Rui Araújo <ruiaraujo012@gmail.com>
2020-03-17 15:22:37 +0000

    :construction: User model added

---
+++ server/models/users.js
@@ -0,0 +1,37 @@
+const Sequelize = require("sequelize");
+
+const User = sequelize.define(
+  "Users",
+  {
+    // attributes
+    id: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    username: {
+      type: Sequelize.STRING(50),
+      allowNull: false
+    },
+    team: {
+      type: Sequelize.ENUM("Instinct", "Mystic", "Valor", "Harmony"),
+      allowNull: false,
+      defaultValue: "Harmony"
+    },
+    level: {
+      type: Sequelize.SMALLINT.UNSIGNED,
+      allowNull: false,
+      defaultValue: 0,
+      validate: {
+        min: 0,
+        max: 40
+      }
+    },
+    passwordHash: {
+      type: Sequelize.TEXT,
+      allowNull: true
+    }
+  },
+  {}
+);
